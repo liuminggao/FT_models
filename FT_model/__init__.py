d8cfcdb492c2e8d20fae7a33cb0ade0c8e5a835f
liuminggao <minghigh@qq.com>
2018-08-23 16:02:05 +0800

    new `get_batches` function

---
+++ FT_model/__init__.py
@@ -0,0 +1,3 @@
+#-*- coding: utf-8 -*-
+
+from FT_model.learner import FTConvLearner
--- FT_model/utils.py
+++ FT_model/utils.py
@@ -1,1 +1,14 @@
 # -*- coding: utf-8 -*-
+
+from keras import preprocessing
+
+DEFAULT_TFMS = dict(rotation_range=20.0,
+                    width_shift_range=0.1,
+                    height_shift_range=0.1,
+                    zoom_range=0.2)
+
+
+def get_train_batches(path, target_size, batch_size=32, shuffle=True, tfms=None):
+    gen = preprocessing.image.ImageDataGenerator(**tfms)
+    batches = gen.flow_from_directory(path, target_size=target_size, batch_size=batch_size, shuffle=shuffle)
+    return gen, batches
