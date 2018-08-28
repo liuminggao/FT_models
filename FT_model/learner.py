a9b0f2ba36cb9ec40e9f5ca70f9b86847bfc3983
liuminggao <minghigh@qq.com>
2018-08-28 18:48:50 +0800

    fix keras model to estimator api bug

--- FT_model/learner.py
+++ FT_model/learner.py
@@ -103,9 +103,10 @@ class FTConvLearner:
         self._build_model()
 
     def finetuning(self, batches, valid_batches, epochs):
-        self.model.fit_generator(batches, steps_per_epoch=batches.n // batches.batch_size,
-                                 validation_data=valid_batches, validation_steps=valid_batches.n // valid_batches.batch_size,
-                                 epochs=epochs)
+        with tf.Session() as sess:
+            self.model.fit_generator(batches, steps_per_epoch=batches.n // batches.batch_size,
+                                     validation_data=valid_batches, validation_steps=valid_batches.n // valid_batches.batch_size,
+                                     epochs=epochs)
 
     def save(self, path):
         os.makedirs(path)
