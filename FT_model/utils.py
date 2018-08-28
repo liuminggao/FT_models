dcfa9ac31ce37a50bf917fa885ee2680c98a83ce
liuminggao <minghigh@qq.com>
2018-08-28 17:53:40 +0800

    fix prepare bug

--- FT_model/utils.py
+++ FT_model/utils.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from keras import preprocessing
+from keras.applications import xception
 
 DEFAULT_TFMS = dict(rotation_range=20.0,
                     width_shift_range=0.1,
@@ -10,8 +11,8 @@ DEFAULT_TFMS = dict(rotation_range=20.0,
 
 def get_batches(path, target_size, batch_size=32, shuffle=True, tfms=None):
     if tfms is not None:
-        gen = preprocessing.image.ImageDataGenerator(**tfms)
+        gen = preprocessing.image.ImageDataGenerator(**tfms, preprocessing_function=xception.preprocess_input)
     else:
-        gen = preprocessing.image.ImageDataGenerator(tfms)
+        gen = preprocessing.image.ImageDataGenerator(tfms, preprocessing_function=xception.preprocess_input)
     batches = gen.flow_from_directory(path, target_size=target_size, batch_size=batch_size, shuffle=shuffle)
     return batches
